35aff5ab8fd9e18e6cbd33f94bcca6d32e97497a
Richard Quadling <RQuadling@GMail.com>
2015-12-15 14:33:45 +0000

    Cleanup after bootstrap is found to be working! Yay!

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,2 @@
 <?php
-echo sprintf('Is Composer loaded? %s%s', class_exists('\Composer\Autoload\ClassLoader', false) ? 'Yes' : 'No', PHP_EOL);
 require_once dirname(__DIR__).'/vendor/autoload.php';
-echo sprintf('Is Composer loaded? %s%s', class_exists('\Composer\Autoload\ClassLoader', false) ? 'Yes' : 'No', PHP_EOL);
